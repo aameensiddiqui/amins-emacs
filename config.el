dcd2d1b3965af094c83bd9bb89bc38889f7bf42e
aameensiddiqui <aameensddq@gmail.com>
2025-06-04 15:20:38 +0530

    some changes

--- config.el
+++ config.el
@@ -5,10 +5,38 @@
 (scroll-bar-mode 0)
 (setq default-frame-alist (cons '(undecorated . t) 
                                 (assq-delete-all 'undecorated default-frame-alist)))
-(ido-mode 1)
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode t)
 (set-frame-font "11" nil t)
 
 (global-hl-line-mode t)
 (setq scroll-conservatively 100)
+
+(setq ido-enable-flex-matching nil)
+(setq ido-create-new-buffer 'always)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(global-set-key (kbd "C-x b") 'ido-switch-buffer)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(setq ibuffer-expert t)
+
+;; autocomplete brackets
+(electric-pair-mode 1)
+
+;; which-key package
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+;; smex package config
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; enable disabled commands
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
